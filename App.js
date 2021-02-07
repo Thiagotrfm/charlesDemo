9ca57d98b77e6ae2aa8162e1f1ef94ae93be2d56
thiagorfm <t_moreira@outlook.com>
2021-02-07 15:13:36 -0300

    chore(project): init project

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+} from 'react-native';
+
+import Quote from './src/scenes/quote/indesx';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Quote />
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
